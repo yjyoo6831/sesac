92f9a7f4ae8ce492a577105fbdaf842461286ec8
yjyoo6831 <cenabro@naver.com>
2024-05-27 19:00:05 +0900

    javascript variable

---
+++ 04_javascript/js_start.js
@@ -0,0 +1,1 @@
+console.log("링크방식")
---
+++ 04_javascript/js_variable.js
@@ -0,0 +1,16 @@
+var a;
+console.log(a);
+a=2;
+console.log(a);
+var a2=4; // 변수 선언 및 초기화 
+console.log(a2);
+
+var a2=5;
+console.log(a2);
+
+let b;
+console.log(b)
+b=7;
+console.log(b);
+b2=78;
+console.log(b2);
