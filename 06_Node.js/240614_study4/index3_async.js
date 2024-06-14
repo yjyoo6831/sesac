c9ed9b60d515662bb52c67df1f50295e82ffc677
yjyoo6831 <cenabro@naver.com>
2024-06-14 18:47:18 +0900

    nodemon install,get,post,form

--- 06_Node.js/240614_study4/index3_async.js
+++ 06_Node.js/240614_study4/index3_async.js
@@ -1,3 +1,4 @@
+// 비동기처리의 3번째 방법: async/await
 async function f1() {
     return 1;
 }
@@ -37,26 +38,70 @@ function fetchFruits() {
 }
 
 // 1. promise then()
-fetchFruits()
-    .then(function (f) {
-        console.log(f);
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+// fetchFruits()
+//     .then(function (f) {
+//         console.log(f);
+//     })
+//     .catch(function (err) {
+//         console.log(err);
+//     });
 
-// 2. async/await
-async function printItems(){
-    try {
-        const fruits = await fetchFruits();
-        // const fruits = fetchFruits();
-        console.log(fruits);
+// // 2. async/await
+// async function printItems(){
+//     try {
+//         const fruits = await fetchFruits();
+//         // const fruits = fetchFruits();
+//         console.log(fruits);
        
-    } catch (error) {
-        console.log(error);    
+//     } catch (error) {
+//         console.log(error);    
 
-    } 
+//     } 
+// }
+// printItems();
+
+///////////////////////////////////////////
+let product;
+let price;
+
+function goMart() {
+    console.log('마트에 가서 어떤 음료를 살지 고민한다..');
 }
-printItems();
 
-// console.log('next');
+function pickDrink() {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('고민 끝');
+            product = '제로 콜라';
+            price = 6000;
+            // resolve();
+            if (price <= 3000) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 3000);
+    });
+}
+
+function pay() {
+    console.log(`상품명: ${product} // 가격: ${price}`);
+}
+
+function nopay() {
+    console.log('금액 부족ㅜㅜ');
+  }
+
+async function exec() {
+    try {
+        // 장점) 함수의 실행 순서가 명확히 보인다!!
+        goMart();
+        await pickDrink(); // 시간이 걸리는 pickDrink() 함수의 작업을 await 키워드로 인해 기다려줌
+        pay();
+    } catch (err) {
+        nopay();
+    }
+}
+
+exec();
+
