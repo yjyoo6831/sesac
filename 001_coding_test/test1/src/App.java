7c5ca4070f406fd3d54d06aef5cd605095bc984f
yjyoo6831 <cenabro@naver.com>
2024-06-18 22:14:35 +0900

    coding test_java

---
+++ 001_coding_test/test1/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args)  {
+
+
+        
+        String a = "77 77 7777";
+        String[] array = a.split(" ");
+        
+        for (i)
+
+        System.out.println("a : "+a+"array : "+Arrays.toString(array));
+
+    }
+}
