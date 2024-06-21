a8b2eb32adf7cc34a301de48798793fdf58642e3
yjyoo6831 <cenabro@naver.com>
2024-06-21 18:20:51 +0900

    sql basic grammer

---
+++ 07_MySQL/join.sql
@@ -0,0 +1,25 @@
+/* join : 여러 테이블을 합쳐서 정보 조회
+
+*/
+-- cross join 
+select * from customer,orders;
+
+-- where 절을 이용해 조인 조건 추가 -- 밑 두개는 같다  (inner join)
+select count(*) from customer, orders 
+    where customer.custid = orders.custid;
+
+select * from customer inner join orders
+    on customer.custid = orders.custid;
+
+-- "고객 이름"과 고객이 주문한 "상품명", "상품 가격" 조회
+select c.custid, o.prodname, o.price from customer c , orders o where c.custid = o.custid;
+
+
+select c.custid, o.prodname, sum( from customer c , orders o where c.custid = o.custid;
+
+select  c.custname, sum(price* amount) as 'total_price'
+from customer c ,orders o
+where c.custid = o.custid
+group by custname 
+order by total_price ;
+
