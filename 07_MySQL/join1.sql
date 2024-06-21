3aabc2a53fdbb703c7420abf2b3ffd9c8222eb97
yjyoo6831 <cenabro@naver.com>
2024-06-21 19:39:02 +0900

    sql basic grammer

--- 07_MySQL/join1.sql
+++ 07_MySQL/join1.sql
@@ -25,3 +25,26 @@ select * from teaches;
 -- 이 때 , 조건이 설정되지 않은 inner join 은 cross join 을 해버린다. 
 select * from instructor join teaches;
 select * from instructor,teaches;
+-- 
+select * from instructor i join teaches t on i.id = t.id;
+
+delete from teaches where id<=2;
+insert into instructor values (3, 'mark', '수학', 75000);
+insert into instructor values (4, 'tom', '심리', 90000);
+insert into teaches values (3, '인공지능', '봄', '2022');
+insert into teaches values (4, '사회심리', '가을', '2023');
+insert into teaches values (5, '네트워크', '봄', '2022');
+insert into teaches values (6, '알고리즘', '가을', '2023');
+
+select * from teaches;
+
+-- left outer join 
+select * from instructor i left outer join teaches t on i.id = t.id;
+
+-- right outer join 
+select * from instructor i right outer join teaches t on i.id = t.id;
+
+-- full outer join  ( mysql 에서는 full outer join 을 지원 X) 대신 union 을 사용한다.
+select * from instructor i left outer join teaches t on i.id = t.id
+union
+select * from instructor i right outer join teaches t on i.id = t.id;
