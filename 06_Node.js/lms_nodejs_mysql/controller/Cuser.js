62787e51a2f21f2681da92bb4e7820fc60e0f645
yjyoo6831 <cenabro@naver.com>
2024-06-27 18:48:53 +0900

    lms practice : complete join

--- 06_Node.js/lms_nodejs_mysql/controller/Cuser.js
+++ 06_Node.js/lms_nodejs_mysql/controller/Cuser.js
@@ -17,12 +17,33 @@ exports.getSignup = (req,res) =>{
 // 회원가입 버튼 클릭시 
 exports.postCreateUser = (req,res) =>{
     // views/signup.ejs 
-    console.log("req.body",req.body);
-    // res.render('signup');
+    console.log("req.body > ",req.body);
+    // req.body > { userid: '12', pw: '1', name: '1' }
+    mUser.postCreateUser(req.body,(result)=>{
+        console.log("controller/Cuser.js postCreateUser >> ", result);
+
+        res.send({
+            userid:req.body.userid,
+            pw:req.body.pw,
+            name:req.body.name
+        })
+    })
 };
 
-
-// // 로그인 버튼 클릭시 
-// exports.createUser = (req,res) =>{
-//     res.render('signin');
-// };
+//로그인 화면 불러오기
+exports.getSignin = (req,res) =>{
+    res.render('signin');
+}
+//로그인 화면 조회 (로그인 일치 조회)
+exports.postCheckUser = (req,res) =>{
+    console.log("check req.body  > ",req.body);
+    // req.body > { userid: '12', pw: '1', name: '1' }
+    mUser.postCheckUser(req.body.userid,req.body.pw,(result)=>{
+        console.log("controller/Cuser.js postCheckUser >> ", result);
+        res.send('test');
+        // res.send({
+        //     userid:req.body.userid,
+        //     pw:req.body.pw,
+        // })
+    })
+}
