728deb9c2f3639e01c10172aedb7d84c49e51e67
yjyoo6831 <cenabro@naver.com>
2024-07-11 15:47:37 +0900

    sesac-project-1/model

---
+++ 000_project/sesac-project-1/models/Musery.js
@@ -0,0 +1,40 @@
+
+
+const USERSMODEL = (sequelize, DataTypes) => {
+    
+    const USERS = sequelize.define('users',
+        {
+            user_Id: {
+                type: DataTypes.STRING(50),
+                primaryKey: true,
+                allowNull: false,
+                autoIncrement: false,
+            },
+            user_name: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+            profile_img: {
+                type: DataTypes.STRING(255),
+                allowNull: true,
+            },
+            user_pw: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            birth_day: {
+                type: DataTypes.DATE,
+                allowNull: false,
+            },
+        },
+        // param3: 모델 옵션 정의
+        {
+            freezeTableName: true, // 테이블 명 고정 
+            // timestamps: false, // 데이터가 추가되고 수정된 시간을 자동으로 컬럼을 만들어서 기록
+        }
+    );
+
+    return USERS;
+}
+
+module.exports = USERSMODEL;
