14b7dcca4578c1799ea007e7c6cd16fc4bb898f2
yjyoo6831 <cenabro@naver.com>
2024-07-23 19:07:15 +0900

    240723

--- 000_project/sesac-project-1/middleware/uploadRecipeImg.js
+++ 000_project/sesac-project-1/middleware/uploadRecipeImg.js
@@ -21,14 +21,14 @@ const uploadFile = multer({
         attributes: ['recipe_num']
       });
 
-      const recipeNum = recipe ? recipe.recipe_num + 1 : 1;
+      // const recipeNum = recipe ? recipe.recipe_num + 1 : 1;
+      const recipeNum = recipe ? recipe.recipe_num + 1 : "error";
       const ext = path.extname(file.originalname);
       
       let fileCount = 0;
-      for (const key in req.files) {
-        fileCount += req.files[key].length;
-      }
-      done(null, path.basename(`${recipeNum}-img` + fileCount , ext) + ext); // 저장할 파일명
+      // 파일 이름 생성
+      const filename = `${recipeNum}-` + path.basename(file.originalname, ext) + ext;
+      done(null, filename); // 저장할 파일명
     } catch (error) {
       console.error(error);
       done(error);
@@ -39,6 +39,49 @@ limits: { fileSize: 100 * 1024 * 1024 }, //업로드 크기 제한
 });
 
 exports.upload = uploadFile.fields([
+    { name: "main_image" },
+    { name: "sub_imgs_1" },
+    { name: "sub_imgs_2" },
+    { name: "sub_imgs_3" },
+    { name: "sub_imgs_4" },
+    { name: "sub_imgs_5" },
+    { name: "sub_imgs_6" },
+    { name: "sub_imgs_7" },
+    { name: "sub_imgs_8" },
+    { name: "sub_imgs_9" },
+    { name: "sub_imgs_10" },
+]);
+
+const uploadPatch = multer({
+  storage: multer.diskStorage({
+  destination(req, file, done) {
+    done(null, "uploads/recipe"); // 파일을 저장할 경로
+  },
+  filename: async (req, file, done) => {
+    try{
+      const recipe = await Recipes.findOne({
+        order: [[ 'createdAt', 'DESC' ]],
+        attributes: ['recipe_num']
+      });
+
+      // const recipeNum = recipe ? recipe.recipe_num + 1 : 1;
+      const recipeNum = recipe ? recipe.recipe_num : "error";
+      const ext = path.extname(file.originalname);
+      
+      let fileCount = 0;
+      // 파일 이름 생성
+      const filename = `${recipeNum}-` + path.basename(file.originalname, ext) + ext;
+      done(null, filename); // 저장할 파일명
+    } catch (error) {
+      console.error(error);
+      done(error);
+    }
+  }
+  }),
+limits: { fileSize: 100 * 1024 * 1024 }, //업로드 크기 제한
+});
+
+exports.uploadpatch = uploadPatch.fields([
     { name: "main_image" },
     { name: "sub_imgs_1" },
     { name: "sub_imgs_2" },
--- 000_project/sesac-project-1/routes/Rrecipe.js
+++ 000_project/sesac-project-1/routes/Rrecipe.js
@@ -18,14 +18,14 @@ router.get('/count/:recipe_num', getLikesCount);
 // get /recipe 레시피 "작성 화면" 보여주기
 router.get("/write", controller.getRecipeWrite);
 
-// get 레시피 수정 페이지
-router.get("/write/:recipe_num", controller.getRecipeUpdate);
- 
 // post 레시피 작성 페이지에서 "저장" 버튼 클릭시
 router.post("/write", mdware.upload, controller.postRecipeWrite);
 
+// get 레시피 수정 페이지
+router.get("/write/:recipe_num", controller.getRecipeUpdate);
+
 // patch 레시피 수정 페이지에서 "수정하기" 버튼 클릭시
-router.post("/update", mdware.upload, controller.patchRecipe);
+router.post("/update", mdware.uploadpatch, controller.patchRecipe);
 
 // delete /recipe/read?recipe_num= "삭제" 버튼 클릭시 
 router.delete("/read", controller.deleteRecipe);
