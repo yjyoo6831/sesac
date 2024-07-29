0bd0fdbd2137091af0d18adbdb9095056aecddd0
yjyoo6831 <cenabro@naver.com>
2024-07-29 18:02:54 +0900

    button

--- 08_React/240729-props/src/App.js
+++ 08_React/240729-props/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 import ClassComponent from './ClassComponent';
 import FunctionComponent from './FunctionComponent';
 
@@ -13,6 +14,9 @@ function App() {
       {/* <FunctionComponent name={3}></FunctionComponent> 3으로 지정하면 나오긴 하지만, 자료형 정확하게 입력하라는 에러가 난다. */} 
       <FunctionComponent name={'SeSAC 6기'}></FunctionComponent>
       <FunctionComponent name='SeSAC 6기'></FunctionComponent>
+      <hr></hr>
+
+      <Button link='https://www.google.com'>Google바로가기</Button>
     </div>
   )
 }
