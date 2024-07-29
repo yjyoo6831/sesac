4281dd83647761f9198311978079bc0df110aa08
yjyoo6831 <cenabro@naver.com>
2024-07-29 15:06:44 +0900

    React type

---
+++ 08_React/240729-first-app/test-app/src/Clock.js
@@ -0,0 +1,9 @@
+function Clock(){
+    return(
+        // toLocaleTimeString() : 사용자의 문화권에 맞는 시간 표기법으로 시간을 리턴
+        <div>
+            <h1>현재 시간은 {new Date().toLocaleTimeString()} 입니다.</h1>
+        </div>
+    )
+}
+export default Clock;
