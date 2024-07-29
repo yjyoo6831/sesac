c8f2270e2f34847dea937ca46f607bd45e419589
yjyoo6831 <cenabro@naver.com>
2024-07-29 17:48:40 +0900

    Prototypes

---
+++ 08_React/240729-props/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import ClassComponent from './ClassComponent';
+import FunctionComponent from './FunctionComponent';
+
+function App() {
+  return (
+    <div className='App'>
+      <ClassComponent />
+      <ClassComponent name='SeSAC 6기'>
+      </ClassComponent>
+      <hr></hr>
+      {/* <FunctionComponent name={3}></FunctionComponent> 3으로 지정하면 나오긴 하지만, 자료형 정확하게 입력하라는 에러가 난다. */} 
+      <FunctionComponent name={'SeSAC 6기'}></FunctionComponent>
+      <FunctionComponent name='SeSAC 6기'></FunctionComponent>
+    </div>
+  )
+}
+
+export default App;
