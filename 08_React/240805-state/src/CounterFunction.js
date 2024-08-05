061d4651d97378423882595105276b2b1daddf48
yjyoo6831 <cenabro@naver.com>
2024-08-05 15:30:07 +0900

    react state, lms assigment

--- 08_React/240805-state/src/CounterFunction.js
+++ 08_React/240805-state/src/CounterFunction.js
@@ -1,8 +1,18 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react'
 
 const CounterFunction = (props) => {
+    const [number, setNumber] = useState(0);
+    const onClickEnter = () => {
+        setNumber(number + 1);
+    }
+
+    const { value } = props;
   return (
-    <div>CounterFunction</div>
+    <div>
+        <h1>{number}</h1>
+        <button onClick={onClickEnter}>{value}</button>
+        <button onClick={onClickEnter}>Plus 1</button>
+    </div>
   )
 }
 
