a7e3d9b7a5e71cecebf6f952a0366aaf708b2a13
yjyoo6831 <cenabro@naver.com>
2024-08-05 15:30:07 +0900

    react state, lms assigment

--- 08_React/240805-state/src/App.js
+++ 08_React/240805-state/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Counter from './Counter';
+import CounterFunction from './CounterFunction';
+import SayFunction from './SayFunction';
+
 function App() {
   return (
     <div className="App">
       <Counter/>
-      <hr></hr>
-      
+      <hr />
+
+      <SayFunction/>
+      <hr />
+
+      <CounterFunction value={"Plus 1"}/>
     </div>
   );
 }
