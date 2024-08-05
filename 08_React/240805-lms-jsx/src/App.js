aafa4cc957a3a668579593636dec9bc956208115
yjyoo6831 <cenabro@naver.com>
2024-08-05 15:17:08 +0900

    react state, lms assigment

---
+++ 08_React/240805-lms-jsx/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    name='두부',
+    animal='이구아나'
+
+    // <h2>반려동물 이름 : {name} \n {name}은 {animal}입니다. </h2>
+
+  );
+}
+
+export default App;
---
+++ 08_React/240805-state/src/CounterFunction.js
@@ -0,0 +1,9 @@
+import React , { useState } from 'react'
+
+const CounterFunction = (props) => {
+  return (
+    <div>CounterFunction</div>
+  )
+}
+
+export default CounterFunction
