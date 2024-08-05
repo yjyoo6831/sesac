b2f18a2d72539903bd422d6ec623806e81e4477f
yjyoo6831 <cenabro@naver.com>
2024-08-05 16:48:42 +0900

    event

---
+++ 08_React/240805-event/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import SyntheticEvent from './SyntheticEvent';
+import ClassBind from './ClassBind';
+function App() {
+  return (
+    <div className="App">
+      <SyntheticEvent />
+      <ClassBind />
+    </div>
+  );
+}
+
+export default App;
