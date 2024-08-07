cc7cd84d40aa1c648fe461ebfd83d75f231b4c1b
yjyoo6831 <cenabro@naver.com>
2024-08-07 14:35:09 +0900

    slack-total-pratice

--- 08_React/240806-slack-assignment-total-practice/src/App.js
+++ 08_React/240806-slack-assignment-total-practice/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Prac2 from './Prac2';
+import Prac from './Prac';
 
 function App() {
   return (
     <div className="App">
-      
+      <Prac2></Prac2>
+      <hr></hr>
+      <Prac></Prac>
     </div>
   );
 }
