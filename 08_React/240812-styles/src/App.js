481345dc3dee54778636e14893039464e5c34dd4
yjyoo6831 <cenabro@naver.com>
2024-08-12 19:56:53 +0900

    240812

---
+++ 08_React/240812-styles/src/App.js
@@ -0,0 +1,23 @@
+
+import CssModuleComponent from './CssModuleComponent';
+import LmsQuiz2 from './LmsQuiz2';
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>React styling</h1>
+      {/* <CssModuleComponent></CssModuleComponent>
+      <hr></hr>
+      <SassComponent></SassComponent>
+      <hr></hr>
+      <StyledComponent></StyledComponent> */}
+      <LmsQuiz2></LmsQuiz2>
+    </div>
+
+  );
+}
+
+export default App;
