ab72ad94b19b5a6fa6606069b741d93cfccadd35
yjyoo6831 <cenabro@naver.com>
2024-08-12 15:46:56 +0900

    lms Quiz 1,2

--- 08_React/240807-lms-map-filter/src/Quiz1.js
+++ 08_React/240807-lms-map-filter/src/Quiz1.js
@@ -1,66 +1,91 @@
-import React , { useState } from 'react'
+import React, { useState } from 'react';
 
 export default function Quiz1() {
-    const [input, setInput] = useState('','');
+    const [inputs, setInputs] = useState(
+        { 
+            username: '' ,
+            email: '' ,
+        }
+    );
+    const {username,email} =inputs;
 
-    const [infoOb,setInfo]=useState([
-        {
-        name:'코디',
-        email:'codi@gmail.com'},
-        {
-            name:'거북이',
-            email:'turtle@gmail.com'},
-        ]);
     const stateHandler = (e) => {
-        console.log(">> ",e);
-        
-    	setInfo({
-            ...infoOb,
-            [e.target.name]: e.target.value,
-            [e.target.email]: e.target.value
-        })
-    }
+        const { name, value } = e.target;
+        // console.log('inputs >> ', e.target.username);
+        console.log('e.target.value >> ', value);
+        setInputs(prevInputs=>({
+            ...prevInputs,
+            [name]:value,
+        }));
 
-    const addInfo = () => {
-        // Q. input 이 빈 값이라면 Infobet 상태가 변경되지 않도록 하기
-        console.log('>>> ', setInput);
-
-        // if ((infoOb.name.trim().length === 0 ) || (input.email.trim().length === 0 )) {
-        //     alert('빈 칸입니다 입력값을 넣어주세요');
-        //     return;
-        // }
-
-        // * concat
-        // - 기존 배열을 변경하지 않고, 주어진 배열이나 값들을 새로운 배열로 결합하여 반환.
-
-        // ex)
-        // let arr1=[1,2,3];
-        // let arr2=[5,6,7];
-        // let arr3=arr1.concat(arr2);  // 리액트는 불변성 유지를 위함.
-
-        //console.log(arr3); // [1,2,3,5,6,7];
-
-        const newInfo = infoOb({
-            name: input.name,
-            email: input.email
-        });
-        setInfo(input); 
-        setInput(''); // 입력 칸 초기화
     };
 
-  return (
-    <>
-        <h1>Quiz1</h1>
-        <input type="text" name="name" id="" placeholder='이름' value={input.name} onChange={stateHandler}/>
-        <input type="email" name="email" id="" placeholder='이메일' value={input.email} onChange={stateHandler}/>
-        <button type="button" onClick={addInfo}>등록</button>
-        <br></br>
+    const [infoOb, setInfo] = useState([
+        {
+            id:1,
+            username: '코디',
+            email: 'codi@gmail.com',
+        },
+        {
+            id:2,
+            username: '거북이',
+            email: 'turtle@gmail.com',
+        },
+    ]);
+    // console.log('infoOb.username > ',infoOb[0].username)
+    // console.log('infoOb.email > ',infoOb.email)
+    const addInfo = () => {
+        if ((username.trim().length === 0 ) || (email.trim().length === 0 )) {
+            alert('빈 칸입니다 입력값을 넣어주세요');
+            return;
+        }
+        
+        setInfo(prevInfo => [...prevInfo, {username,email}]);
+        setInputs({username:'',email:''}); 
+    };
+ //글자 삭제
+ const deleteAlpha = (targetId) => {
+
+    console.log('targetId > ', targetId); // targetId : 삭제될 요소의 Id
+    // console.log('delete > ',)
+
+    const newAlpha = infoOb.filter((v) => v.id !== targetId);
+    setInfo(newAlpha);
+};
+
+// 엔터키 입력하면 추가되도록
+const activeEnter = (e) => {
+    console.log('e >> ', e);
+
+    if (e.key === 'Enter') {
+        addInfo();
+    }
+};
+    return (
+        <>
+            <h1>Quiz1</h1>
+            <input
+                type="text"
+                name="username"
+                placeholder="이름"
+                onChange={stateHandler} />
+            <input 
+                type="email" 
+                name="email" 
+                placeholder="이메일" 
+                onChange={stateHandler} />
+
+            <button type="button" onClick={addInfo} onKeyDown={(e) => activeEnter(e)}>
+                등록
+            </button>
+            <br></br>
+            <ol>
             {
-                infoOb.map( (n) => (
-                <div> {n.name} : {n.email} </div>
+                infoOb.map( (n,idx) => (
+                    <li key={idx} onDoubleClick={() => deleteAlpha(n.id)}> {n.username} : {n.email} </li>
             ))
             }
-        
-    </>
-  )
+            </ol>
+        </>
+    );
 }
---
+++ 08_React/240807-lms-map-filter/src/Quiz3.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Quiz3() {
+    return <div>Quiz3</div>;
+}
