6324a42699d0caa09c6fa9b6b0921dda1641be3a
yjyoo6831 <cenabro@naver.com>
2024-08-12 14:22:30 +0900

    react-hook-form

--- 08_React/240809-hooks/src/App.js
+++ 08_React/240809-hooks/src/App.js
@@ -6,6 +6,7 @@ import UseCallbackEx from './UseCallbackEx';
 import UseCallbackEx2 from './UseCallbackEx2';
 import UseMemoEx from './UseMemoEx';
 import UseReducerEx from './UseReducerEx';
+import Form from './components/react-hook-form/Form';
 
 function App() {
   UseTitle('!!!')
@@ -14,8 +15,10 @@ function App() {
       {/* <UseMemoEx></UseMemoEx> */}
       {/* <UseCallbackEx></UseCallbackEx>
       <UseCallbackEx2 postId={7}></UseCallbackEx2> */}
-      <UseReducerEx></UseReducerEx>
-      <Faq></Faq>
+      {/* <UseReducerEx></UseReducerEx>
+      <Faq></Faq> */}
+
+      <Form></Form>
     
     </div>
   );
