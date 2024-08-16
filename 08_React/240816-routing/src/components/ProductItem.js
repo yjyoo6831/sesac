57d52955daafaec6124f0e5ae10b599fa046f1d5
yjyoo6831 <cenabro@naver.com>
2024-08-16 18:51:59 +0900

    240816

---
+++ 08_React/240816-routing/src/components/ProductItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function ProductItem({product}) {
+    console.log('product >>> ',product);
+        // {userid, title, body}
+  return (
+    <>
+        <Link to={`/products/${product.id}`}>
+            <ul>
+                <li>상품명 : {product.title}</li>
+                <li>상세 설명 : {product.body}</li>
+            </ul>
+        </Link>
+        <hr></hr>
+    </>
+  )
+}
