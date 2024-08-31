ea112bc9755c8219ac2ef9f9749fad0d427ee580
yjyoo6831 <cenabro@naver.com>
2024-08-31 22:59:50 +0900

    backup

---
+++ 000_project/sesac-project-2/models/product/LikesModel.js
@@ -0,0 +1,47 @@
+// 좋아요 모델 정의
+const LikesModel = (sequelize, DataTypes) =>{
+    const Likes = sequelize.define(
+        'Likes', 
+        { 
+        // 좋아요 인덱스
+        likesId:{
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        // 상품 인덱스 
+        productId:{
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        // 유저 인덱스
+        userId:{
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            onUpdate: 'CASCADE',
+            onDelete: 'CASCADE'
+        },
+        likesCount:{
+            type:DataTypes.BIGINT,
+            allowNull:false,
+            defaultValue:0
+        }
+    },
+    {
+        freezeTableName : true, 
+        timestamps : true, 
+    }
+);
+Likes.associate = function (models) {
+    Likes.belongsTo(models.Product,{foreignKey:"productId", sourceKey:"productId"});
+    Likes.belongsTo(models.User,{foreignKey:"userId", sourceKey:"userId"});
+}
+    return Likes;
+}
+
+
+module.exports=LikesModel;
+
---
+++ 000_project/sesac-project-2/service/likesService.js
@@ -0,0 +1,94 @@
+const { Product, Likes } = require('../models/Index');
+var sequelize = require('sequelize');
+
+exports.getLikes = async (req, res) => {
+    try {
+        const { productId } = req.query;
+        const likes = await Likes.findOne({
+            where: {
+                productId,
+            },
+            attributes: [[sequelize.fn('SUM', sequelize.col('likesCount')), 'totalLike']],
+            raw: true
+        });
+        console.log("likes >> ", likes.totalLike);
+        if (likes.totalLike) {
+            res.status(400).json({"totalLike" : likes.totalLike});
+        } else {
+            res.send('해당 상품은 좋아요 개수가 조회되지 않습니다.');
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'getLikes 서버 오류', err: err.message });
+    }
+};
+
+exports.postLikes = async (req, res) => {
+    console.log("postlikes >>> --------------");
+
+    try {
+        console.log('req.query > ', req.query);
+        const { productId, userId } = req.query;
+        const userlikes = await Likes.findOne({
+            where: {
+                productId,
+                userId,
+            },
+            attributes: ['likesCount'],
+            raw: true
+        });
+        console.log('b4 likesCount >> ', userlikes);
+        if (userlikes) {
+            console.log('product, user 가 table 에 존재함.')
+
+            const isAlreadyLike = await Likes.findOne({
+                where: {
+                    productId,
+                    userId,
+                },
+                attributes: ['likesCount'],
+                raw: true
+            });
+
+            console.log("isAlreadyLike > ", isAlreadyLike);
+            if (isAlreadyLike.likesCount === 1) { // 유저 좋아요 1일 경우
+                console.log(`유저 좋아요가 1이므로 0으로 바뀜`);
+                await Likes.destroy({
+                    where: { productId, userId}
+                })
+            } else { // 유저 좋아요 0일 경우
+                console.log(`유저 좋아요가 0이므로 1으로 바뀜`);
+                await likesCreate(productId, userId, 1);
+            }
+        } else {
+            const isUser = await Likes.findOne({
+                where: {
+                    productId,
+                    userId,
+                },
+                attributes: ['userId'],
+                raw: true
+            });
+            if (!isUser) {
+                console.log(`product 에 대한 좋아요는 있지만,
+                    ${userId}의 값은 없으므로 새로 생성한다.`);
+                await likesCreate(productId, userId, 1);
+
+            } 
+        }  // else
+        res.send(`${userId}번 유저가 ${productId}번 상품에 
+                좋아요를 눌렀습니다.`);
+    } catch (err) {
+        res.status(500).json({ message: 'postLikes 서버 오류', err: err.message });
+    }
+};
+
+function likesCreate(productId, userId, likesCount) {
+    Likes.create(
+        { productId, userId, likesCount },
+        {
+            where: {
+                productId,
+                userId,
+            },
+        })
+}
