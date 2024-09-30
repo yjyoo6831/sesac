e70ba296a3865cfaabecfe7fe9f4f2fc6a3cfd62
yjyoo6831 <cenabro@naver.com>
2024-09-30 19:42:47 +0900

    java generic

--- java-language/src/_05_class/_abstract/Square.java
+++ java-language/src/_05_class/_abstract/Square.java
@@ -1,7 +1,7 @@
 package _05_class._abstract;
 
 public class Square extends Shape {
-    // 추상클래스는 new 연산자로 "직접 생서자 호출은 불가능"하지만,
+    // 추상클래스는 new 연산자로 "직접 생성자 호출은 불가능"하지만,
     // 자식 객체 생성시 `super(...)` 연산자 호출(부모 클래스의 생성자)해서 추상클래스 객체 생성
     // 자식클래스에서 super로 호출하여 추상클래스의 생성자 접근은 가능하다.
     public Square(String color) { super(color); }
