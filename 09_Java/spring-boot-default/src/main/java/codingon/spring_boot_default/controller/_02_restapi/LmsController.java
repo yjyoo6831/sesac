a514286be17441762f94f8d3b2a6e56425ac6814
yjyoo6831 <cenabro@naver.com>
2024-10-08 15:17:58 +0900

    실습) 동적 폼 전송 실습

---
+++ 09_Java/spring-boot-default/src/main/java/codingon/spring_boot_default/controller/_02_restapi/LmsController.java
@@ -0,0 +1,93 @@
+package codingon.spring_boot_default.controller._02_restapi;
+
+import codingon.spring_boot_default.vo.UserVO;
+import codingon.spring_boot_default.vo.LmsVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LmsController {
+    @GetMapping("/")
+    public String getReq(){
+        return "_02_restapi/lmsres";
+    }
+    // lms 과제-1
+    @GetMapping("/introduce/{name}")
+    public String introduce(@PathVariable String name, Model model) {
+        System.out.println("param(name) = " + name);
+        model.addAttribute("name", name);
+        return "_02_restapi/lmsres";
+    }
+
+    @GetMapping("/introduce2")
+    public String introduce2(@RequestParam(value="name") String name, @RequestParam(value="age") int age, Model model) {
+        System.out.println("query(name) = " + name);
+        System.out.println("query(age) = " + age);
+
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "_02_restapi/lmsres";
+    }
+
+    @PostMapping("/post/lms")
+    @ResponseBody
+    public String postLms(@RequestParam String name, @RequestParam String gender, @RequestParam String date,
+                          @RequestParam String interest, Model model) {
+
+        System.out.println("이름 : " + name +
+                "\n성별 : " + gender +
+                "\n생년월일 : " + date + "\n관심사 : " + interest
+        );
+        model.addAttribute("name", name);
+        model.addAttribute("gender", gender);
+        model.addAttribute("date", date);
+        model.addAttribute("interest", interest);
+        return "이름 : " + name +  "<br>" +
+                "성별 : " + gender + "<br>" +
+                "생년월일 : " + date + "<br>" +
+                "관심사 : " + interest;
+    }
+
+    @PostMapping("/post/axios")
+    @ResponseBody
+    public String postAxios(@RequestBody UserVO userVO){
+//        System.out.println("userVO = " + userVO);
+        System.out.println("[POST] userVO.name = " + userVO.getName());
+        System.out.println("[POST] userVO.gender = " + userVO.getGender());
+        System.out.println("[POST] userVO.getDate = " + userVO.getDate());
+        System.out.println("[POST] userVO.interest = " + userVO.getInterest());
+
+        return userVO.getName() + "회원가입 성공";
+    }
+
+    // 회원 관리 시스템
+    // 회원가입
+    @PostMapping("/join")
+    @ResponseBody
+    public String postJoin(@RequestBody LmsVO lmsVO){
+        return lmsVO.getName() + " " + lmsVO.getPassword();
+    }
+    // 로그인
+    @GetMapping("/login")
+    @ResponseBody
+    public String getLogin(@ModelAttribute LmsVO lmsVO){
+        lmsVO.add
+        return lmsVO.getName() + " " + lmsVO.getPassword();
+    }
+    // 회원정보수정
+    @PutMapping("/update")
+    @ResponseBody
+    public String putUpdate(@RequestBody LmsVO lmsVO){
+        return lmsVO.getName() + " " + lmsVO.getPassword();
+    }
+    // 회원정보삭제
+    @DeleteMapping("/deleted")
+    public String delUser(@RequestBody LmsVO lmsVO){
+        return lmsVO.getName() + " " + lmsVO.getPassword();
+    }
+
+
+
+
+}
---
+++ 09_Java/spring-boot-default/src/main/java/codingon/spring_boot_default/vo/LmsVO.java
@@ -0,0 +1,9 @@
+package codingon.spring_boot_default.vo;
+
+import lombok.Getter;
+
+@Getter
+public class LmsVO {
+    private String name;
+    private String password;
+}
