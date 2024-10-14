62327c6491b94d02605a196a66803673d010260a
yjyoo6831 <cenabro@naver.com>
2024-10-14 19:19:06 +0900

    fix: main page scroll

--- java-language/src/_00_lms_pratice/_interface_240930/Car.java
+++
@@ -1,8 +0,0 @@
-package _00_lms_pratice._interface_240930;
-
-public class Car extends Vehicle {
-    @Override
-    public void move() {
-        System.out.println("도로를 따라 이동중");
-    }
-}
--- java-language/src/_05_class/_access_modifier/_pack1/B.java
+++
@@ -1,6 +0,0 @@
-package _05_class._access_modifier._pack1;
-
-public class B {
-    A a;
-    // A클래스(default)와 B 클래스(public) 은 같은 패키지 이므로 접근 가능
-}
