f91966e449dc77eff19adbfdc4962bca9d48ff36
yjyoo6831 <cenabro@naver.com>
2024-10-16 14:26:54 +0900

    jpa create,update,delete

--- 09_Java/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/controller/UserController.java
+++ 09_Java/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/controller/UserController.java
@@ -4,10 +4,7 @@ import codingon.spring_boot_jpa.dto.UserDTO;
 import codingon.spring_boot_jpa.service.UserService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,30 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDTO getUser(@PathVariable Long id){
         return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public UserDTO createUser(@RequestBody UserDTO userDTO){
+        userService.createUser(userDTO);
+        return userDTO;
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO){
+        //  id 를 userDTO 의 값으로 수정하겠다.
+        userService.updateUser(id, userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    // 1. 사용자 이름으로 n명 조회
+    @GetMapping("/byUsername")
+    public List<UserDTO> getUserByUsername(@RequestParam String username){
+        return userService.getUserByUsername(username);
     }
 }
--- 09_Java/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/repository/UserRepository.java
+++ 09_Java/spring-boot-jpa/src/main/java/codingon/spring_boot_jpa/repository/UserRepository.java
@@ -4,6 +4,8 @@ import codingon.spring_boot_jpa.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 // 해당 인터페이스가 Repository 컴포넌트임을 명시
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -12,4 +14,11 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // - 제네릭 첫번째 타입: 관련 테이블
     // - 제네릭 두번째 타입: 관련 테이블 PK의 타입
+
+    // case1. repository 메서드 사용
+    // 1. 사용자 이름으로 n 명 조회
+    List<User> findByUsername(String username); //  select * from user where username  = ?
+    // 2. 검색어 보냈을 때 사용자 이름/ 이메일에 특정 문자열이 포함된 모든 사용자 n명 찾기
+
+    // case2. @Query 어노테이션 사용
 }
