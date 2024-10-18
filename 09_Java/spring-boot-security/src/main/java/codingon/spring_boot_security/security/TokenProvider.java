94f70f648fe0579e6880204443afc0a7727683ad
yjyoo6831 <cenabro@naver.com>
2024-10-18 18:52:36 +0900

    spring-boot security-authorization

--- 09_Java/spring-boot-security/src/main/java/codingon/spring_boot_security/security/TokenProvider.java
+++ 09_Java/spring-boot-security/src/main/java/codingon/spring_boot_security/security/TokenProvider.java
@@ -1,12 +1,14 @@
 package codingon.spring_boot_security.security;
 // security 패키지 : 인증/인가에 관련된 클래스 모음
 
+import codingon.spring_boot_security.config.jwt.JwtProperties;
 import codingon.spring_boot_security.entity.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -18,7 +20,10 @@ import java.util.Date;
 // 사용자 정보를 받아서 JWT를 생성하는 클래스
 public class TokenProvider {
     // 비밀키 생성
-    private static final String SECRET_KEY = "sesac-springboot-231234";
+//    private static final String SECRET_KEY = "sesac-springboot-231234";
+    // [after] JwtProperties 클래스 이용해 설정 파일 값 꺼내오기
+    @Autowired
+    private JwtProperties jwtProperties;
 
     // create() : JWT 생성
     public String create(UserEntity userEntity){
@@ -29,10 +34,12 @@ public class TokenProvider {
         // JWT 토큰 생성
          return Jwts.builder()
                  // header(token 구성 요소 중 하나) 에 들어갈 내용 및 서명을 하기 위한 설정
-                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+//                 .signWith(SignatureAlgorithm.HS512, SECRET_KEY)
+                    .signWith(SignatureAlgorithm.HS512, jwtProperties.getSecretKey())
                  // payload에 들어갈 내용
                  .setSubject(String.valueOf(userEntity.getId())) // 토큰 제목
-                 .setIssuer("demo app") // iss: 토큰 발급자
+//                 .setIssuer("demo app") // iss: 토큰 발급자
+                 .setIssuer(jwtProperties.getIssuer())
                  .setIssuedAt(new Date()) // iat: 토큰이 발급된 시간
                  .setExpiration(expiryDate) // exp: 토큰 만료 시간
                  // 토큰 생성
@@ -47,7 +54,8 @@ public class TokenProvider {
         // - 두 값이 불일치(위조)하면 예외
         // - 결국 필요한 것은 userId 이므로 getBody를 부른다.
         Claims claims = Jwts.parser() // 파서 생성
-                .setSigningKey(SECRET_KEY) // 서명 검증을 위해 비밀키 입력
+//                .setSigningKey(SECRET_KEY) // 서명 검증을 위해 비밀키 입력
+                .setSigningKey(jwtProperties.getSecretKey())
                 .parseClaimsJws(token) // 토큰을 파싱하고 서명 검증 -> 토큰 위조  예외 발생
                 .getBody(); // 검증된 토큰의 본문(claims)을 가져옴
 
