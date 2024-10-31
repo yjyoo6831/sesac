67f5d08812af17659a5770fdffe38c4c147dd297
yjyoo6831 <cenabro@naver.com>
2024-10-31 17:12:17 +0900

    ㄹfieat:리뷰 목록 조회 완료 / fix:리뷰 작성 controller 진행중

--- 000_project/sesac-project-3/my.sql
+++ 000_project/sesac-project-3/my.sql
@@ -12,6 +12,10 @@ select * from place where location like "%성북%";
 
 select * from review;
 select * from user;
+
+select * from review r 
+join place p on r.place_id = p.place_id;
+
 insert into user values(false,false,now(),now(),1,'test1','test1','010-2222-3333','abc@naver.com','1234','a.png');
 insert into user values(false,false,now(),now(),2,'test2','test2','010-2222-1111','abc2@naver.com','1234','a.png');
 
